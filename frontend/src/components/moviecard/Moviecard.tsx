848285e047af0b2855e6394d70f8bb49eff38ab7
Biellms <biell.mendes8@gmail.com>
2022-03-10 13:48:52 -0300

    Navigation buttons

--- frontend/src/components/moviecard/Moviecard.tsx
+++ frontend/src/components/moviecard/Moviecard.tsx
@@ -1,4 +1,5 @@
 import MovieScore from 'components/moviescore/Moviescore';
+import { Link } from 'react-router-dom';
 
 function MovieCard() {
 
@@ -14,9 +15,15 @@ function MovieCard() {
         <div>
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
+
                 <h3>{movie.title}</h3>
+
                 <MovieScore />
-                <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+
+                <Link to={`/form/${movie.id}`}>
+                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                </Link>
+
             </div>
         </div>
     );
--- frontend/src/pages/listing/Listing.tsx
+++ frontend/src/pages/listing/Listing.tsx
@@ -35,7 +35,6 @@ function Listing() {
                     </div>
                 </div>
             </div>
-
         </>
 
     );
