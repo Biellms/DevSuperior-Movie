22b9b50b090aead2d9b638ad3607aee2107dba36
Biellms <biell.mendes8@gmail.com>
2022-03-14 12:57:52 -0300

    First request

--- frontend/src/pages/listing/Listing.tsx
+++ frontend/src/pages/listing/Listing.tsx
@@ -1,7 +1,15 @@
+import axios from "axios";
 import MovieCard from "components/moviecard/Moviecard";
 import Pagination from "components/pagination/Pagination";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+    //Forma errada
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    .then(response => {
+        console.log(response.data);
+    })
 
     return (
 
