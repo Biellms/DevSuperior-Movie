fec2185a11c24990d44269eeed2d73d663f77ddd
Biellms <biell.mendes8@gmail.com>
2022-03-13 21:52:50 -0300

    Att build

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,29 +1,24 @@
-import MovieScore from 'components/moviescore/Moviescore';
-import { Link } from 'react-router-dom';
+import MovieScore from "components/MovieScore";
+import { Link } from "react-router-dom";
+import { Movie } from "types/movie";
 
-function MovieCard() {
 
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+type Props = {
+    movie: Movie;
+}
+
+function MovieCard( { movie } : Props) {
+
 
     return (
         <div>
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
-
                 <h3>{movie.title}</h3>
-
-                <MovieScore />
-
+                <MovieScore  count={movie.count} score={movie.score}/>
                 <Link to={`/form/${movie.id}`}>
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                 </Link>
-
             </div>
         </div>
     );
